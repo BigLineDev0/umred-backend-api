8892f2cc7aec8d2a01f51cef7b4e0fd609a40ae6
BigLineDev0 <alioudiallo.py@gmail.com>
2026-09-18 13:49:44 +0000

    configuration setting pour le deployement

--- config/settings.py
+++ config/settings.py
@@ -149,3 +149,16 @@ FRONTEND_URL = env('FRONTEND_URL', default='http://localhost:4200')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# --- Sécurité production ---
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
